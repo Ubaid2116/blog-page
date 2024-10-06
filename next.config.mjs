1ad33f6e022f3abd72d0ab2ff9ea755dc1e9ff0a
Ubaid2116 <ukubaid74@gmail.com>
2024-10-07 01:49:27 +0500

    update code

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        pathname: "/**",
-      },
-    ],
-  },
-};
-
-export default nextConfig;
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "cdn.sanity.io",
+          pathname: "/**", // This allows any path under the specified hostname
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
